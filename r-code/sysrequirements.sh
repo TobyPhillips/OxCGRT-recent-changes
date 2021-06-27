c27bf2becdbd7deb100abeab12bc2a39ba7b98cd
Toby Phillips <59369581+TobyPhillips@users.noreply.github.com>
2021-06-27 22:28:10 +1000

    Create sysrequirements.sh

---
+++ r-code/sysrequirements.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get install libcurl4-openssl-dev
+sudo apt install -y libudunits2-0 libudunits2-dev
+sudo apt install libgdal-dev
+Rscript -e 'install.packages("remotes")'
+Rscript -e 'remotes::install_cran(c("rmarkdown", "tidyverse", "lubridate", "rnaturalearth", "viridis", "knitr", "xtable", "data.table", "kableExtra", "here", "curl"
+))'
+Rscript -e 'remotes::install_github("hadley/dplyr", build_vignettes = FALSE)'
